e4923af59fd5df480ceee822685cca3443575fb4
mskhim <mskhim1@naver.com>
2024-12-06 11:07:29 +0900

    [수정] Mypage session방어

--- jspStudy/src/main/java/co/kh/dev/login/controler/MemberLoginCheckServlet.java
+++ jspStudy/src/main/java/co/kh/dev/login/controler/MemberLoginCheckServlet.java
@@ -11,26 +11,32 @@ import co.kh.dev.login.model.MemberVO;
 @WebServlet({ "/memberloginCheckServlet.do" })
 public class MemberLoginCheckServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private void processRequest(HttpServletRequest request, HttpServletResponse response) {
-				HttpSession session= request.getSession();
-				MemberDAO mDAO =new MemberDAO();
-				String id = request.getParameter("id");
-				String pwd = request.getParameter("pwd");
-				MemberVO mvo = new MemberVO(null, id, pwd); 
-				mvo = mDAO.selectLoginCheckDB(mvo);
-				try {
-					if (mvo.getRegdate()!=null) {
-						session.setAttribute("id", mvo.getId());
-						session.setAttribute("pwd", mvo.getPwd());
-						session.setAttribute("name", mvo.getName());
-						response.sendRedirect("/jspStudy/member/memberLogin.jsp");
-					} else {
-						response.sendRedirect("/jspStudy/member/memberLogin.jsp?error=1");
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
+		HttpSession session = request.getSession();
+		MemberDAO mDAO = new MemberDAO();
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		MemberVO mvo = new MemberVO(null, id, pwd);
+		MemberVO idCheckmvo = mDAO.selectByIdDB(mvo);
+		MemberVO loginCheckmvo = mDAO.selectLoginCheckDB(mvo);
+		try {
+			if (idCheckmvo.getRegdate() != null) {
+
+				if (loginCheckmvo.getRegdate() != null) {
+					session.setAttribute("id", mvo.getId());
+					session.setAttribute("pwd", mvo.getPwd());
+					session.setAttribute("name", mvo.getName());
+					response.sendRedirect("/jspStudy/member/memberLogin.jsp");
+				} else {
+					response.sendRedirect("/jspStudy/member/memberLogin.jsp?error=1");//비밀번호일치 x
 				}
+			} else {
+				response.sendRedirect("/jspStudy/member/memberLogin.jsp?error=2");//아이디일치 x
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
